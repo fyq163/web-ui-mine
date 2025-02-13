5f946a8e0eab16c57512dc7a8b148c27ee4c0703
fyq163 <fyq163@126.com>
2025-02-13 21:39:41 +0800

    Add Moonshot and Tongyi Provider

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -24,6 +24,7 @@ PROVIDER_DISPLAY_NAMES = {
     "gemini": "Gemini"
 }
 
+
 def get_llm_model(provider: str, **kwargs):
     """
     获取LLM 模型
@@ -142,10 +143,28 @@ def get_llm_model(provider: str, **kwargs):
         return AzureChatOpenAI(
             model=kwargs.get("model_name", "gpt-4o-mini"),
             temperature=kwargs.get("temperature", 0.0),
-            api_version="2024-05-01-preview",
+            api_version="2024-02-15-preview",
             azure_endpoint=base_url,
             api_key=api_key,
         )
+    elif provider == "moonshot":
+        return ChatOpenAI(
+            model=kwargs.get("model_name", "gpt-4o"),
+            temperature=kwargs.get("temperature", 0.0),
+            base_url="https://api.moonshot.ai/v1",
+            api_key=api_key,
+            http_client=client
+        )
+    # TODO: Add Moonshot provider
+    elif provider == "tongyi":
+        return ChatOpenAI(
+            model=kwargs.get("model_name", "gpt-4o"),
+            temperature=kwargs.get("temperature", 0.0),
+            base_url="https://api.tongyi.ai/v1",
+            api_key=api_key,
+            http_client=client
+        )
+    # TODO: Add TongYi qwen provider
     else:
         raise ValueError(f"Unsupported provider: {provider}")
 
@@ -155,11 +174,14 @@ model_names = {
     "anthropic": ["claude-3-5-sonnet-20240620", "claude-3-opus-20240229"],
     "openai": ["gpt-4o", "gpt-4", "gpt-3.5-turbo", "o3-mini"],
     "deepseek": ["deepseek-chat", "deepseek-reasoner"],
-    "gemini": ["gemini-2.0-flash-exp", "gemini-2.0-flash-thinking-exp", "gemini-1.5-flash-latest", "gemini-1.5-flash-8b-latest", "gemini-2.0-flash-thinking-exp-01-21","gemini-2.0-pro-exp-02-05"],
+    "gemini": ["gemini-2.0-flash-exp", "gemini-2.0-flash-thinking-exp", "gemini-1.5-flash-latest",
+               "gemini-1.5-flash-8b-latest", "gemini-2.0-flash-thinking-exp-01-21", "gemini-2.0-pro-exp-02-05"],
     "ollama": ["qwen2.5:7b", "llama2:7b", "deepseek-r1:14b", "deepseek-r1:32b"],
     "azure_openai": ["gpt-4o", "gpt-4", "gpt-3.5-turbo"],
-    "mistral": ["pixtral-large-latest", "mistral-large-latest", "mistral-small-latest", "ministral-8b-latest"]
+    "mistral": ["pixtral-large-latest", "mistral-large-latest", "mistral-small-latest", "ministral-8b-latest"],
+    # "": []
 }
+
 
 # Callback to update the model name dropdown based on the selected provider
 def update_model_dropdown(llm_provider, api_key=None, base_url=None):
@@ -178,6 +200,7 @@ def update_model_dropdown(llm_provider, api_key=None, base_url=None):
     else:
         return gr.Dropdown(choices=[], value="", interactive=True, allow_custom_value=True)
 
+
 def handle_api_key_error(provider: str, env_var: str):
     """
     Handles the missing API key error by raising a gr.Error with a clear message.
@@ -187,6 +210,7 @@ def handle_api_key_error(provider: str, env_var: str):
         f"💥 {provider_display} API key not found! 🔑 Please set the "
         f"`{env_var}` environment variable or provide it in the UI."
     )
+
 
 def encode_image(img_path):
     if not img_path:
@@ -199,7 +223,7 @@ def encode_image(img_path):
 def get_latest_files(directory: str, file_types: list = ['.webm', '.zip']) -> Dict[str, Optional[str]]:
     """Get the latest recording and trace files"""
     latest_files: Dict[str, Optional[str]] = {ext: None for ext in file_types}
-    
+
     if not os.path.exists(directory):
         os.makedirs(directory, exist_ok=True)
         return latest_files
@@ -214,8 +238,10 @@ def get_latest_files(directory: str, file_types: list = ['.webm', '.zip']) -> Di
                     latest_files[file_type] = str(latest)
         except Exception as e:
             print(f"Error getting latest {file_type} file: {e}")
-            
+
     return latest_files
+
+
 async def capture_screenshot(browser_context):
     """Capture and encode a screenshot"""
     # Extract the Playwright browser instance
