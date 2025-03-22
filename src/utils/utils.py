b1f47fa493a5cbe5602535d77e5e6a08bb4bdc45
fyq163 <fyq163@126.com>
2025-03-22 17:52:46 +0800

    modify order

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -177,10 +177,9 @@ model_names = {
     "openai": ["gpt-4o", "gpt-4", "gpt-3.5-turbo", "o3-mini"],
     "deepseek": ["deepseek-chat", "deepseek-reasoner"],
     "ollama": ["llama3.1:latest", "qwen2.5:14b", "qwen2.5:32b", "qwen2.5-coder:14b", "qwen2.5-coder:32b", "llava-llama3:latest", "deepseek-r1:14b", "llama3.2-vision:latest"],
-    "mistral": ["pixtral-large-latest", "mistral-large-latest", "mistral-small-latest", "ministral-8b-latest"],
     "google": ["gemini-2.0-flash", "gemini-2.0-flash-thinking-exp", "gemini-1.5-flash-latest",
                "gemini-1.5-flash-8b-latest", "gemini-2.0-flash-thinking-exp-01-21", "gemini-2.0-pro-exp-02-05"],
-
+    "mistral": ["pixtral-large-latest", "mistral-large-latest", "mistral-small-latest", "ministral-8b-latest"],
     "azure_openai": ["gpt-4o", "gpt-4", "gpt-3.5-turbo"],
     "alibaba": ["qwen-plus", "qwen-max", "qwen-turbo", "qwen-long"],
     "moonshot": ["moonshot-v1-32k-vision-preview", "moonshot-v1-8k-vision-preview"],
